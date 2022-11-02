e6068c35eae7329bb9e9d1ba6178d4bf8ca1112e
David Gómez <103974083+dabydat@users.noreply.github.com>
2022-11-02 15:20:47 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,37 @@
+
+# Asignar Fechas a Personas
+
+Un pequeño reto realizado con Python: Asignar fechas a personas, dado un año, y un mes, y un diccionario de personas.
+
+Existen dos casos:
+
+- Caso 1: Que se asigne una fecha, es decir, una persona por día
+- Caso 2: Que se asignen fechas a las personas en un mismo mes de forma equitativa
+
+Estos dos casos fueron tomados en cuenta al realizar el script.
+
+Este pequeño script recorre un diccionario, y asigna fechas de forma aleatoria a las personas.
+Si se excede la cantidad de personas de la cantidad de días, se puede modificar para que tome
+el mes siguiente, o vuelva a tomar los del mes actual.
+
+Otro punto a tomar en cuenta es: sólo los días de semana son tomados en cuenta.
+
+
+
+
+## Documentación
+
+ - [Método monthrange() de python](https://testdriven.io/tips/0ab72d80-5f92-49fb-98c5-e3e7cafab671/)
+ - [Módulo Datetime en python](https://www.programiz.com/python-programming/datetime)
+ - [Módulo Random de Python](https://www.programiz.com/python-programming/modules/random)
+
+
+## Autores
+
+- [@dabydat](https://github.com/dabydat)
+
+
+## Licencia
+
+- [MIT](https://choosealicense.com/licenses/mit/)
+
---
+++ asignar-fechas-personas.py
@@ -0,0 +1,139 @@
+from calendar import monthrange
+from datetime import datetime,date
+import random
+
+personas = {
+    0: {"nombre":'PRUEBA - 1', "fecha": ''},
+    1: {"nombre":'PRUEBA - 2', "fecha": ''},
+    2: {"nombre":'PRUEBA - 3', "fecha": ''},
+    3: {"nombre":'PRUEBA - 4', "fecha": ''},
+    4: {"nombre":'PRUEBA - 5', "fecha": ''},
+    5: {"nombre":'PRUEBA - 6', "fecha": ''},
+    6: {"nombre":'PRUEBA - 7', "fecha": ''},
+    7: {"nombre":'PRUEBA - 8', "fecha": ''},
+    8: {"nombre":'PRUEBA - 9', "fecha": ''},
+    9: {"nombre":'PRUEBA - 10', "fecha": ''},
+    10:{"nombre":'PRUEBA - 11', "fecha": ''},
+    11:{"nombre":'PRUEBA - 1', "fecha": ''},
+    12:{"nombre":'PRUEBA - 13', "fecha": ''},
+    13:{"nombre":'PRUEBA - 14', "fecha": ''},
+    14:{"nombre":'PRUEBA - 15', "fecha": ''},
+    15:{"nombre":'PRUEBA - 16', "fecha": ''},
+    16:{"nombre":'PRUEBA - 17', "fecha": ''},
+    17:{"nombre":'PRUEBA - 18', "fecha": ''},
+    18:{"nombre":'PRUEBA - 19', "fecha": ''},
+    19:{"nombre":'PRUEBA - 20', "fecha": ''},
+    20:{"nombre":'PRUEBA - 21', "fecha": ''},
+    22:{"nombre":'PRUEBA - 22', "fecha": ''},
+    23:{"nombre":'PRUEBA - 23', "fecha": ''},
+    24:{"nombre":'PRUEBA - 24', "fecha": ''},
+    25:{"nombre":'PRUEBA - 25', "fecha": ''},
+    26:{"nombre":'PRUEBA - 26', "fecha": ''},
+    27:{"nombre":'PRUEBA - 27', "fecha": ''},
+    28:{"nombre":'PRUEBA - 28', "fecha": ''},
+    29:{"nombre":'PRUEBA - 29', "fecha": ''},
+}
+
+# Función que obtiene los dias dado un mes, y un año
+def getDays(anio, mes, nuevo_cantidad_dias = 0):
+    hoy = date(anio, mes, 1)
+    anio = hoy.year
+    mes = hoy.month
+    cantidad_dias_mes = monthrange(anio, mes)[1]
+
+    dias_del_mes = [date(anio, mes, dia) for dia in range(1, cantidad_dias_mes+1)]
+    dias_del_mes = gettingWeekdays(dias_del_mes)
+
+    if nuevo_cantidad_dias != 0:
+        dias_del_mes = gettingWeekdays(dias_del_mes)
+        finalDays = slice(nuevo_cantidad_dias)
+        dias_del_mes = dias_del_mes[finalDays]
+
+    return dias_del_mes
+
+# Funcion que asigna la fecha a la persona seleccionada
+def addPerson(j, day, list_days):
+    try:
+        if day != None:
+            personas[j]['fecha'] = day.strftime("%d/%m/%Y")
+            if day in list_days:
+                list_days.remove(day)
+    except Exception as e:
+        return e
+
+# Función que escoge un día al azar
+def randomDay(days):
+    randomDay = random.choice(days)
+    return randomDay
+
+# Función que elimina los fines de semana, y retorna los días de semana
+def gettingWeekdays(monthDays):
+    weekdays = []
+    for i in monthDays:
+        weekday = i.weekday()
+        
+        if weekday < 5:
+            weekdays.append(i)
+
+    return weekdays
+
+# Función que cuenta la cantidad de personas sin fecha
+def getNonDate(dictionary, value):
+    counter = 0
+    for x, y in dictionary.items():
+        if y[value] == "":
+            counter = counter + 1
+    return counter
+
+#Función que asigna la fecha de forma aleatoria
+def assignDate(keysDictList, daysList):
+    cantidad = 0
+    nuevo_cantidad_dias = 1
+    fecha_inicio = daysList[0]
+    for i in keysDictList:
+        try:
+            if daysList != []:
+                diaRandom = randomDay(daysList)
+                addPerson(i, diaRandom, daysList)
+            else:
+                if cantidad == 0:
+                    cantidad = getNonDate(personas, 'fecha')
+
+                    anio = fecha_inicio.year
+                    # Sumarle uno al mes es para que sea una pesona por día, si le quitamos el '+1' asignará personas equitativamente en el mes
+                    mes = fecha_inicio.month+1
+
+                    if mes > 12:
+                        anio = anio + 1
+                        mes = 1
+                    
+                    list_new_days = getDays(anio, mes, cantidad)
+        
+                new_day = randomDay(list_new_days)
+                addPerson(i, new_day, list_new_days)
+                
+                nuevo_cantidad_dias = nuevo_cantidad_dias + 1
+        except Exception as e:
+            if e:
+                print(e)
+
+# Función para ordenar las personas por fecha
+def orderByDate(dictionary, orderValue):
+    dictionary_values = list(dictionary.values())
+    mylist = sorted(dictionary_values, key = lambda x:datetime.strptime(x[orderValue], '%d/%m/%Y'), reverse=True)
+    mylist.reverse()
+    return mylist
+
+# Función para obtener las llaves de un diccionario
+def getKeysOfDict(dictionary):
+    keysListed = list(dictionary.keys())
+    random.shuffle(keysListed)
+    return keysListed
+
+personasList = getKeysOfDict(personas)
+dias_del_mes = getDays(2022, 12)
+
+assignDate(personasList, dias_del_mes)
+mylist = orderByDate(personas, 'fecha')
+for i in mylist:
+    print(i["nombre"] + " ---> Fecha: " + i["fecha"])
